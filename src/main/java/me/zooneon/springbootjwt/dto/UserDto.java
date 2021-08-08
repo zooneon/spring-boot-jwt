6693484a7b65450f690d9ed237c0556046cd623d
zooneon <snsxhdz@naver.com>
2021-08-08 20:04:38 +0900

    로그인 api 구현

---
+++ src/main/java/me/zooneon/springbootjwt/dto/UserDto.java
@@ -0,0 +1,30 @@
+package me.zooneon.springbootjwt.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String username;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @NotNull
+    @Size(min = 3, max = 100)
+    private String password;
+
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String nickname;
+}
