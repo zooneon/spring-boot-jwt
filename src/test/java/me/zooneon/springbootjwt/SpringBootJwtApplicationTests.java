05706de3d4f785274bc93e0d7bee54fd50aa2f53
zooneon <snsxhdz@naver.com>
2021-08-08 15:08:52 +0900

    프로젝트 생성

---
+++ src/test/java/me/zooneon/springbootjwt/SpringBootJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package me.zooneon.springbootjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
